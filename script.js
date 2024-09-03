f4f78b73db582bd74cf0c55328c17543b6025efa
Uci Chatrinada <ucichatrinada@gmail.com>
2024-09-03 19:57:24 +0700

    membuat function

--- script.js
+++ script.js
@@ -3,10 +3,23 @@
 [1,2]           // 3
 [5]             // 5
 []              // array kosong*/
-let array = [1,2,3,4,5];
-let total =[0]
-for (let i=0;i<array.length;i++){
-    total += array[i];
-    
+/*let total = [0];
+for (let i=1;i<=5;i++){
+    total +=i;
+    console.log(total)
     }
 
+let total2 = 1+2+3+4+5;
+console.log();*/
+
+function penjumlahan(Array){
+    if (Array.length===0){
+        return "array kosong";
+    }
+    let total3=0;
+    for (let i = 0; i <Array.length;i++){
+        total +=arr[i];
+    }
+    return total3;
+}
+console.log(penjumlahan([1,2,3,4,5]));
