f164d8c80269874569f6b733a7cd79d89099d1d6
Uci Chatrinada <ucichatrinada@gmail.com>
2024-09-03 19:36:50 +0700

    membuat let

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 [5]             // 5
 []              // array kosong*/
 let array = [1,2,3,4,5];
+let total =[0]
 for (let i=0;i<array.length;i++){
-    total
-}
+    total += array[i];
+    
+    }
+
