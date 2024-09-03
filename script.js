6dc62e201fde79aeef99bc1977e2ffa0f028674f
Uci Chatrinada <ucichatrinada@gmail.com>
2024-09-03 19:31:12 +0700

    loading1

---
+++ script.js
@@ -0,0 +1,9 @@
+//Menjumlahkan data pada array // input        // result
+/*[1,2,3,4,5]     // 15
+[1,2]           // 3
+[5]             // 5
+[]              // array kosong*/
+let array = [1,2,3,4,5];
+for (let i=0;i<array.length;i++){
+    total
+}
